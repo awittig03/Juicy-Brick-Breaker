78716c026810308e24a82ade7ffafb968ee98385
awittig03 <143130984+awittig03@users.noreply.github.com>
2023-10-11 20:22:12 -0400

    Done with game!

--- Comet_Container.gd
+++ Comet_Container.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 
 func _physics_process(_delta):
+
 	for c in get_children():
 		if c.modulate.a <= 0 or c.modulate.v <= 0:
 			c.queue_free()
@@ -9,3 +10,4 @@ func _physics_process(_delta):
 		c.modulate.a -= 0.03
 		c.modulate.v -= 0.01
 		c.modulate.h += 0.02
+
